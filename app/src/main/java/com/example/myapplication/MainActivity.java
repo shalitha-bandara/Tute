4af007aa0c014a6c9c8cf71a06e99c67156dd3e8
shalitha-bandara <it19092584@my.sliit.lk>
2020-08-05 20:26:11 +0530

    final project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textview2;
+    private static final String TAG = "Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,30 @@ public class MainActivity extends AppCompatActivity {
 
         textview2 = findViewById(R.id.textView2);
         textview2.setText(R.string.Msg2);
+        Log.i(TAG, "onCreate: ");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
     }
 }
